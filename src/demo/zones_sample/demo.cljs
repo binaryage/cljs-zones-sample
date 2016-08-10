12155775636c28882e0eb0db33d5eb76f5ce3fb5
Antonin Hildebrand <antonin@hildebrand.cz>
2016-08-11 00:01:22 +0200

    using log generates simpler code
    for anyone looking at advanced build output

--- src/demo/zones_sample/demo.cljs
+++ src/demo/zones_sample/demo.cljs
@@ -1,7 +1,6 @@
 (ns zones-sample.demo
   (:require-macros [zones-sample.logging :refer [log]])
-  (:require [clojure.string :as string]
-            [zones-sample.boot :refer [boot!]]
+  (:require [zones-sample.boot :refer [boot!]]
             [zones.core :as zones :include-macros true]))
 
 (boot! "/src/demo/zones_sample/demo.cljs")
@@ -9,10 +8,10 @@
 ; --- MEAT STARTS HERE -->
 ; note: (log ...) expands to (.log js/console ...)
 
-(println "before:" (zones/get v))
+(log "before:" (zones/get v))
 (zones/binding [v "I'm a dynamically bound value in the default zone"]
-               (println "inside:" (zones/get v))
-               (js/setTimeout (zones/bound-fn [] (println "in async call:" (zones/get v))) 500))
-(println "after:" (zones/get v))
+               (log "inside:" (zones/get v))
+               (js/setTimeout (zones/bound-fn [] (log "in async call:" (zones/get v))) 500))
+(log "after:" (zones/get v))
 
 ; <-- MEAT STOPS HERE ---
